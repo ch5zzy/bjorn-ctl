e5949a4525fb1e53440f20c43c282325c69e3afa
Chase Hartsell <cmhartsell@me.com>
2024-03-20 12:02:49 -0500

    Remove transparency from images

--- src/app/util/minify.tsx
+++ src/app/util/minify.tsx
@@ -4,5 +4,5 @@ import sharp, { KernelEnum } from "sharp";
 
 export default async function minify(imgBase64: string, kernel: keyof KernelEnum) {
     const imgSharp = sharp(Buffer.from(imgBase64.split(';base64,').pop() ?? "", "base64"), { animated: true });
-    return "data:image/webp;base64," + (await imgSharp.resize(16, 16, { kernel }).webp({ quality: 100, lossless: true }).toBuffer()).toString("base64");
+    return "data:image/webp;base64," + (await imgSharp.flatten({ background: { r: 0, g: 0, b: 0 } }).resize(16, 16, { kernel }).webp({ quality: 100, lossless: true }).toBuffer()).toString("base64");
 }
